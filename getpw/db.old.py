89422f8f58550f5defc17c5bc1f5f12296de7978
fungleo <web@fengcms.com>
2017-12-22 16:54:38 +0800

    数据库处理改为orm 方式,原sql处理备份为db.old.py

---
+++ getpw/db.old.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+##########################################################################
+# Name:    db.py                                                         #
+# Author:  FungLeo                                                       #
+# Date:    2017-12-20                                                    #
+# Purpose: This program is used to generate simple or complex passwords  #
+# Copy:    for study, prohibition of commercial use                      #
+##########################################################################
+import sqlite3
+import re
+import sys
+from prettytable import PrettyTable
+
+DB_PATH = sys.path[0] + '/passwd.db'
+
+def checkDB(db):
+    db.execute('''SELECT name FROM sqlite_master
+		WHERE type IN ('table','view') AND name NOT LIKE 'sqlite_%'
+		ORDER BY 1''')
+    o = db.fetchall()
+    if len(o) == 0 or not bool(re.search(r'(\'passwd\',)',str(o))):
+        db.execute('''CREATE TABLE passwd (
+            id integer primary key autoincrement,
+            name varchar(255),
+            password varchar(255),
+            time timestamp default current_timestamp
+        )''')
+def insertDb(name,passwd):
+    conn = sqlite3.connect(DB_PATH)
+    c = conn.cursor()
+    checkDB(c)
+    c.execute("INSERT INTO passwd (name,password) VALUES ('" + name + "', '" + passwd + "')");
+    conn.commit()
+    conn.close()
+
+def selectDb(pid,name):
+    conn = sqlite3.connect(DB_PATH)
+    c = conn.cursor()
+    checkDB(c)
+
+    select = "SELECT * from passwd "
+    if name:
+        select += ('where name LIKE \'%' + name + '%\'')
+    if pid:
+        select += ('where id = \'' + str(pid) + '\'')
+    select += 'ORDER BY id DESC'    
+
+    res = list(c.execute(select))
+    
+    if len(res) == 0:
+        print('Info: record is empty')
+    else:
+        x = PrettyTable(['id','name','password','time'])
+        x.align['name'] = 'l'
+        x.padding_width = 1
+        for row in res:
+            x.add_row(list(row))
+        print(x)
+    conn.close()
+
+def deleteDb(pid):
+    conn = sqlite3.connect(DB_PATH)
+    c = conn.cursor()
+    checkDB(c)
+    c.execute('DELETE from passwd where id=' + str(pid) )
+    conn.commit()
+    o = conn.total_changes
+    if o == 0:
+        print('Failure: the password was not found')
+    if o == 1:
+        print('Success: ID ' + str(pid) + ' password has been deleted')
+    conn.close()
--- getpw/db.py
+++ getpw/db.py
@@ -1,73 +1,64 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
-##########################################################################
-# Name:    db.py                                                         #
-# Author:  FungLeo                                                       #
-# Date:    2017-12-20                                                    #
-# Purpose: This program is used to generate simple or complex passwords  #
-# Copy:    for study, prohibition of commercial use                      #
-##########################################################################
-import sqlite3
-import re
+from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 import sys
 from prettytable import PrettyTable
 
-DB_PATH = sys.path[0] + '/passwd.db'
+DB_PATH = 'sqlite:///' + sys.path[0] + '/passwd.db'
+Base = declarative_base()
+metadata = Base.metadata
 
-def checkDB(db):
-    db.execute('''SELECT name FROM sqlite_master
-		WHERE type IN ('table','view') AND name NOT LIKE 'sqlite_%'
-		ORDER BY 1''')
-    o = db.fetchall()
-    if len(o) == 0 or not bool(re.search(r'(\'passwd\',)',str(o))):
-        db.execute('''CREATE TABLE passwd (
-            id integer primary key autoincrement,
-            name varchar(255),
-            password varchar(255),
-            time timestamp default current_timestamp
-        )''')
-def insertDb(name,passwd):
-    conn = sqlite3.connect(DB_PATH)
-    c = conn.cursor()
-    checkDB(c)
-    c.execute("INSERT INTO passwd (name,password) VALUES ('" + name + "', '" + passwd + "')");
-    conn.commit()
-    conn.close()
 
-def selectDb(pid,name):
-    conn = sqlite3.connect(DB_PATH)
-    c = conn.cursor()
-    checkDB(c)
+class Passwd(Base):
+    __tablename__ = 'passwd'
 
-    select = "SELECT * from passwd "
-    if name:
-        select += ('where name LIKE \'%' + name + '%\'')
-    if pid:
-        select += ('where id = \'' + str(pid) + '\'')
-    select += 'ORDER BY id DESC'    
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255))
+    password = Column(String(255))
+    time = Column(DateTime, server_default=text("current_timestamp"))
 
-    res = list(c.execute(select))
+engine = create_engine(DB_PATH)
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+def obj2list(obj):
+    d = []
+    for column in obj.__table__.columns:
+        d.append(str(getattr(obj, column.name)))
+    return d
+def insertDb(n, pw):
+    new_pw = Passwd(name=n,password=pw)
+    session.add(new_pw)
+    session.commit()
+
+def selectDb(pid, name):
+    res = session.query(Passwd)
+
+    x = PrettyTable(['id','name','password','time'])
+    x.align['name'] = 'l'
+    x.padding_width = 1
     
-    if len(res) == 0:
+    if pid:
+        res = res.filter_by(id = pid)
+    elif name:
+        res = res.filter(Passwd.name.like('%' + name + '%'))
+    res = res.all()
+    if not res:
         print('Info: record is empty')
-    else:
-        x = PrettyTable(['id','name','password','time'])
-        x.align['name'] = 'l'
-        x.padding_width = 1
-        for row in res:
-            x.add_row(list(row))
-        print(x)
-    conn.close()
+        exit()
 
+    for i in res:
+        x.add_row( obj2list(i) )
+
+    print(x)
 def deleteDb(pid):
-    conn = sqlite3.connect(DB_PATH)
-    c = conn.cursor()
-    checkDB(c)
-    c.execute('DELETE from passwd where id=' + str(pid) )
-    conn.commit()
-    o = conn.total_changes
-    if o == 0:
-        print('Failure: the password was not found')
-    if o == 1:
+    de = session.query(Passwd).get(pid)
+    if de:
+        session.delete(de)
+        session.commit()
         print('Success: ID ' + str(pid) + ' password has been deleted')
-    conn.close()
+    else:
+        print('Failure: the password was not found')
+
