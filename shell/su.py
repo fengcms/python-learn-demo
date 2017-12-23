313aefa42081b89835fc6694feba9653833058db
fungleo <web@fengcms.com>
2017-12-23 23:45:26 +0800

    恢复原状了，暂时不搞这个质因数了。

--- shell/su.py
+++ shell/su.py
@@ -12,7 +12,7 @@ def isPrime(n):
         i += 1
     return True
 
-def calc(num, arr):
+def calc(num):
     i = 2
     while i*i <= int(num):
     #for i in range(2,int(num**0.5) + 1):
@@ -20,9 +20,8 @@ def calc(num, arr):
             arr.append(i)
             if isPrime(num/i):
                 arr.append(int(num/i))
-                return arr
             else:
-                calc(num/i, arr)
+                calc(num/i)
             break
         i += 1
     return []
@@ -45,4 +44,6 @@ def echo(num, a):
 if __name__ == '__main__':
     print('这是一个计算一个数字的质因数的程序\n请输入您要计算质因数的数字')
     num = checkInput()
-    echo(num, calc(num, []))
+    arr = []
+    calc(num)
+    echo(num, arr)
