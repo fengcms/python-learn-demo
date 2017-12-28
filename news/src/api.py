17a799b748b90a5be941ff2009a5bc3b4324e845
fungleo <web@fengcms.com>
2017-12-28 16:00:22 +0800

    顺利完成post 和 get 操作

--- news/src/api.py
+++ news/src/api.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
-from flask import Flask, request
-
-import db
+from flask import Flask, request, json
+import model
 
 app = Flask(__name__)
 
@@ -12,31 +11,13 @@ def hello_world():
 
 @app.route('/news', methods=['GET', 'POST'])
 def news():
-    print(str(request.form))
     if request.method == 'GET':
-        print('get news')
+        model.readDb()
         return 'get status:200'
     elif request.method == 'POST':
-        r = request.form
-        print(r['title'])
-        print(r['content'])
-        sql_str = '''INSERT INTO article (title, content) VALUES ('{title}','{content}')'''
-        sql_str = sql_str.format(title = r['title'], content = r['content'])
-        print(sql_str)
-        db.insertDb(sql_str)
-        print('post news')
+        model.insertDb(request.form.to_dict())
         return 'post status:200'
 
-#@app.route('/news/<username>')
-#def news(username):
-#    res = 'your username is ' + username
-#    return res
-#
-#@app.route('/id/<int:id>')
-#def id(id):
-#    print(type(id))
-#    res = 'your id is ' + str(id)
-#    return res
 
 if __name__ == '__main__':
     app.run()
---
+++ news/src/model.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import sys
+
+DB_PATH = 'sqlite:///' + sys.path[0] + '/news.db'
+
+Base = declarative_base()
+metadata = Base.metadata
+
+
+class Article(Base):
+    __tablename__ = 'article'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(255))
+    channel_id = Column(Integer)
+    author = Column(String(255))
+    origin = Column(String(255))
+    content = Column(Text)
+    img = Column(String(255))
+    time = Column(DateTime, server_default=text("current_timestamp"))
+
+engine = create_engine(DB_PATH)
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+metadata.create_all(engine)
+
+def row2dict(row):
+    d = {}
+    for column in row.__table__.columns:
+        d[column.name] = str(getattr(row, column.name))
+    return d
+
+def insertDb(dat):
+    new_art = Article(
+        title = dat.get('title'),
+        channel_id = dat.get('channel_id'),
+        author = dat.get('author') or '',
+        origin = dat.get('origin') or '',
+        content = dat.get('content'),
+        img = dat.get('img') or '',
+    )
+    session.add(new_art)
+    session.commit()
+
+def readDb():
+    res = []
+    dat = session.query(Article).all()
+    for i in dat:
+        res.append(row2dict(i))
+    print(res)
