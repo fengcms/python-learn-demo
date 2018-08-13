d056acd12466e1fd560b06acf0a3ec2ec346b003
fungleo <web@fengcms.com>
2018-08-13 14:48:13 +0800

    移除文章系统，准备独立开一个git仓库

--- news/be/process/api_v1_be/after/manages.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: UTF-8 -*-
-
-async def ls (response):
-    for i in response['list']:
-        i.pop('password')
-    return response
--- news/fe-manage/src/App.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <router-view id='app'/>
-</template>
-
-<style lang='scss'>
-  @import 'style/style';
-</style>
